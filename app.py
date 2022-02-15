363b1ad789551ab28725931ccb0ea77a1e61cb88
Teoh TeikToe <ttt@mensa.org.sg>
2022-02-15 13:32:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def index():
         s1 = "The predicted DBS share price using regression is " + str(pred1)
         s2 = "The predicted DBS share price using Decision Tree is " + str(pred2)
         s3 = "The predicted DBS share price using regression is " + str(pred3)
-        return(render_template("index.html", result1 = s1, result2=s2, result3=s3)
+        return(render_template("index.html", result1 = s1, result2=s2, result3=s3))
     else:
         return(render_template("index.html", result1="2", result2="2", result3="2"))
 
