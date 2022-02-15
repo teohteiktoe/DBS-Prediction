30089f6b94051b664bf11c3bc1dc1fb08fd99a89
Teoh TeikToe <ttt@mensa.org.sg>
2022-02-15 11:49:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,17 @@ def index():
     if request.method == "POST":
         rates = request.form.get("rates")
         print(rates)
+        model = joblib.load("DBSReg")
+        pred1 = model.predict([[float(rates)]])
+        model = joblib.load("DBSDT")
+        pred2 = model.predict([[float(rates)]])
         model = joblib.load("DBSNN")
-        pred = model.predict([[float(rates)]])
-        print(pred)
-        s = "The predicted DBS share price is " + str(pred)
-        return(render_template("index.html", result = s))
+        pred3 = model.predict([[float(rates)]])
+        print(pred1)
+        s = "The predicted DBS share price is " + str(pred1)
+        return(render_template("index.html", result1 = s, result2=str(pred2), result3=str(pred3))
     else:
-        return(render_template("index.html", result="2"))
+        return(render_template("index.html", result1="2", result2="2", result3="2"))
 
 if __name__ == "__main__":
     app.run()
