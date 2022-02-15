d4a697a8094cd6d788f565186afdcb9e8b30e244
Teoh TeikToe <ttt@mensa.org.sg>
2022-02-15 12:17:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ import joblib
 def index():
     if request.method == "POST":
         rates = request.form.get("rates")
-        print(rates)
         model1 = joblib.load("DBSReg")
         pred1 = model1.predict([[float(rates)]])
         model2 = joblib.load("DBSDT")
         pred2 = model2.predict([[float(rates)]])
         mode3 = joblib.load("DBSNN")
         pred3 = model3.predict([[float(rates)]])
-        print(pred1)
-        s = "The predicted DBS share price is " + str(pred1)
-        return(render_template("index.html", result1 = s, result2=str(pred2), result3=str(pred3))
+        s1 = "The predicted DBS share price using regression is " + str(pred1)
+        s2 = "The predicted DBS share price using Decision Tree is " + str(pred2)
+        s3 = "The predicted DBS share price using regression is " + str(pred3)
+        return(render_template("index.html", result1 = s1, result2=s2, result3=s3)
     else:
         return(render_template("index.html", result1="2", result2="2", result3="2"))
 
